3444471d6b698513b12c372318f719e5d5c4614b
Vinícius Campos <agstelecombr@gmail.com>
2026-04-24 16:48:11 -0300

    corrigindo banco para nuvem

--- server.js
+++ server.js
@@ -1,71 +1,62 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-const db = new sqlite3.Database('./estoque.db');
+const db = new Database('estoque.db');
 
 // Criar tabela
-db.run(`
+db.prepare(`
 CREATE TABLE IF NOT EXISTS produtos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT,
     quantidade INTEGER
 )
-`);
+`).run();
 
 // Listar produtos
 app.get('/produtos', (req, res) => {
-    db.all('SELECT * FROM produtos', [], (err, rows) => {
-        res.json(rows);
-    });
+    const rows = db.prepare('SELECT * FROM produtos').all();
+    res.json(rows);
 });
 
 // Adicionar produto
 app.post('/produtos', (req, res) => {
     const { nome, quantidade } = req.body;
-    db.run(
-        'INSERT INTO produtos (nome, quantidade) VALUES (?, ?)',
-        [nome, quantidade],
-        function (err) {
-            res.json({ id: this.lastID });
-        }
-    );
+
+    const result = db.prepare(
+        'INSERT INTO produtos (nome, quantidade) VALUES (?, ?)'
+    ).run(nome, quantidade);
+
+    res.json({ id: result.lastInsertRowid });
 });
 
 // Atualizar estoque
 app.put('/produtos/:id', (req, res) => {
     const { quantidade } = req.body;
-    db.run(
-        'UPDATE produtos SET quantidade = ? WHERE id = ?',
-        [quantidade, req.params.id],
-        () => {
-            res.send('Atualizado');
-        }
-    );
+
+    db.prepare(
+        'UPDATE produtos SET quantidade = ? WHERE id = ?'
+    ).run(quantidade, req.params.id);
+
+    res.send('Atualizado');
 });
 
 // Deletar produto
 app.delete('/produtos/:id', (req, res) => {
-    db.run('DELETE FROM produtos WHERE id = ?', [req.params.id], () => {
-        res.send('Deletado');
-    });
+    db.prepare('DELETE FROM produtos WHERE id = ?')
+      .run(req.params.id);
+
+    res.send('Deletado');
 });
 
-app.post('/login', (req, res) => {
-    const { usuario, senha } = req.body;
+const PORT = process.env.PORT || 3000;
 
-    if (usuario === 'admin' && senha === '1234') {
-        res.json({ sucesso: true });
-    } else {
-        res.json({ sucesso: false });
-    }
-});
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+app.listen(PORT, () => {
+    console.log('Servidor rodando');
 });
